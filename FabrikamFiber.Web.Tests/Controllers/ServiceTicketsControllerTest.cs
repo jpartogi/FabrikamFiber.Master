b2badcb7415a9c76d65b47f1342b6f32dd788c9d
Joshua Partogi <joshua.partogi@gmail.com>
2014-02-07 03:07:21 +0700

    Refactor Test to use NSubstitute

--- FabrikamFiber.Web.Tests/Controllers/ServiceTicketsControllerTest.cs
+++ FabrikamFiber.Web.Tests/Controllers/ServiceTicketsControllerTest.cs
@@ -8,47 +8,48 @@
     using FabrikamFiber.Web.Controllers;
     using FabrikamFiber.Web.ViewModels;
     using NUnit.Framework;
-    using NUnit.Mocks;
+    using NSubstitute;
 
     [TestFixture]
     public class ServiceTicketsControllerTest
     {
-        DynamicMock mockCustomerRepo;
-        DynamicMock mockEmployeeRepo;
-        DynamicMock mockServiceTicketRepo;
-        DynamicMock mockLogEntryRepo;
-        DynamicMock mockScheduleItemRepo;
+        ICustomerRepository mockCustomerRepo;
+        IEmployeeRepository mockEmployeeRepo;
+        IServiceTicketRepository mockServiceTicketRepo;
+        IServiceLogEntryRepository mockLogEntryRepo;
+        IScheduleItemRepository mockScheduleItemRepo;
         ServiceTicketsController controller;
-
+        
         [SetUp]
-        public void SetupController()
+        public void SetUp()
         {
-            mockCustomerRepo = new DynamicMock(typeof(ICustomerRepository));
-            mockEmployeeRepo = new DynamicMock(typeof(IEmployeeRepository));
-            mockServiceTicketRepo = new DynamicMock(typeof(IServiceTicketRepository));
-            mockLogEntryRepo = new DynamicMock(typeof(IServiceLogEntryRepository));
-            mockScheduleItemRepo = new DynamicMock(typeof(IScheduleItemRepository));
+            mockCustomerRepo = Substitute.For<ICustomerRepository>();
+            mockEmployeeRepo = Substitute.For<IEmployeeRepository>();
+            mockServiceTicketRepo = Substitute.For<IServiceTicketRepository>();
+            mockLogEntryRepo = Substitute.For<IServiceLogEntryRepository>();
+            mockScheduleItemRepo = Substitute.For<IScheduleItemRepository>();
 
             controller = new ServiceTicketsController(
-                mockCustomerRepo.MockInstance as ICustomerRepository,
-                mockEmployeeRepo.MockInstance as IEmployeeRepository,
-                mockServiceTicketRepo.MockInstance as IServiceTicketRepository,
-                mockLogEntryRepo.MockInstance as IServiceLogEntryRepository,
-                mockScheduleItemRepo.MockInstance as IScheduleItemRepository
+                mockCustomerRepo,
+                mockEmployeeRepo,
+                mockServiceTicketRepo,
+                mockLogEntryRepo,
+                mockScheduleItemRepo
             );
         }
 
-        [Xunit.Fact]
+        [Test]
         public void ScheduleActionReturnsValidViewModel()
         {
-            this.SetupController();
-
             // Arrange
-            mockServiceTicketRepo.SetReturnValue("FindIncluding", new ServiceTicket { ID = 1 });
-            mockEmployeeRepo.SetReturnValue("Find", new Employee { ID = 1 });
+            mockServiceTicketRepo.FindIncluding(1).ReturnsForAnyArgs(new ServiceTicket { ID = 1 });
+            
+            // mockEmployeeRepo.SetReturnValue("Find", new Employee { ID = 1 });
+            mockEmployeeRepo.Find(1).Returns(new Employee { ID = 1 });
+            
             var scheduleItems = new List<ScheduleItem>();
             scheduleItems.Add(new ScheduleItem { ID = 1, });
-            mockScheduleItemRepo.SetReturnValue("AllIncluding", scheduleItems.AsQueryable<ScheduleItem>());
+            mockScheduleItemRepo.AllIncluding().ReturnsForAnyArgs(scheduleItems.AsQueryable<ScheduleItem>());
 
             // Act
             var result = (ViewResult)controller.Schedule(1, 1, 0);
@@ -60,31 +61,25 @@
             Assert.NotNull(model.ServiceTicket);
             controller.ViewBag.StartTime = 0;
         }
-
-        [Xunit.Fact]
+        
+        [Test]
         public void ScheduleActionCorrectlyUpdatesRepositories()
         {
-            this.SetupController();
-
             // Arrange
             var scheduleItems = new List<ScheduleItem>();
             scheduleItems.Add(new ScheduleItem { ServiceTicketID = 1 });
-            //mockScheduleItemRepo.SetReturnValue("get_All", scheduleItems.AsQueryable<ScheduleItem>());
-            mockScheduleItemRepo.SetReturnValue("InsertOrUpdate", null);
-            mockScheduleItemRepo.SetReturnValue("Delete", null);
-            mockScheduleItemRepo.ExpectAndReturn("Save", null);
-
+            mockScheduleItemRepo.All.Returns(scheduleItems.AsQueryable<ScheduleItem>());
+         
             ServiceTicket ticket = new ServiceTicket { ID = 0 };
-            mockServiceTicketRepo.SetReturnValue("Find", ticket);
-            mockServiceTicketRepo.ExpectAndReturn("Save", null);
+            mockServiceTicketRepo.Find(1).Returns(ticket);
 
             // Act
             controller.AssignSchedule(1, 101, 0);
 
             // Assert
             Assert.AreEqual(101, ticket.AssignedToID);
-            mockScheduleItemRepo.Verify();
-            mockServiceTicketRepo.Verify();
+            mockScheduleItemRepo.ReceivedCalls();
+            mockServiceTicketRepo.ReceivedCalls();
         }
     }
 }
